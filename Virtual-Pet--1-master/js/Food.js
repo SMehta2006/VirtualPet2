e5096452b7ac6087e7325c96a7393c4f054c2d47
SMehta2006 <75286744+SMehta2006@users.noreply.github.com>
2021-02-04 14:27:26 -0800

    Add files via upload

---
+++ Virtual-Pet--1-master/js/Food.js
@@ -0,0 +1,25 @@
+class Food{
+    constructor(){
+         foodStock;
+         lastFed;
+
+        this.image = ("images/Milk.png");
+
+    }
+
+    getFoodStock(){
+
+    }
+
+    updateFoodStock(){
+
+    }
+
+    deductFood(){
+
+    }
+
+    display(){
+
+    }
+}
